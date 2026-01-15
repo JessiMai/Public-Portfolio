0027358dd807ca4704ba4f30f1a19679a732f6c9
Jess <44055049+JessiMai@users.noreply.github.com>
2026-01-15 08:02:05 +0000

    Create FormFittingFunctions.py

---
+++ FormFittingFunctions.py
@@ -0,0 +1,10 @@
+# Class for form fitting
+
+class FormFitting:
+  def LorentzianFitting(self, Y, X=[]):
+    if X==[]:
+      X = list(range(len(Y)))
+
+    # L = A/B
+    # B = 1 + ((x-x0)/(reflectionCoefficient/2))**2
+    return A, x0, reflectionCoefficient
